bbfd72794c244ef0a0a1c1f4e1c676e7b077b26b
Flindt <flindt@flindtathome.dk>
2017-07-20 16:13:30 +0200

    added folders

---
+++ portaPrince/src/predictors/historic.py
@@ -0,0 +1,21 @@
+'''
+Created on 20 Jul 2017
+
+@author: flindt
+'''
+
+class HistoricPredictor(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, pdHistoricValues, startDate):
+        self.historicValues = pdHistoricValues
+        self.startDate = startDate
+        
+    def predict(self, n=100):
+        
+        
+        return [0,1,2]
+    
