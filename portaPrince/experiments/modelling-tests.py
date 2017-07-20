a13ae36b359d13cf8e1b293bb003a36c1a74a718
moz <moz@local>
2017-07-20 16:16:27 +0200

    wip stuff

--- portaPrince/experiments/modelling-tests.py
+++ portaPrince/experiments/modelling-tests.py
@@ -28,16 +28,35 @@ def plot_autocorr( df ):
     plt.close()
     return delta_autocorr
 
+def get_binned_data( df, bin_count=10 ):
+    v_max, v_min = df.max(), df.min()
+    bins = [(v_max-v_min)/(bin_count+1)*i+v_min for i in range(bin_count+1)]
+    labels = ["{0} {1:.1f}".format(i, (v_max-v_min)/(bin_count+1)*(i+0.5)+v_min) for i in range(bin_count)]
+
+    categories = pd.cut(df, bins, labels=labels)
+    #print( categories)
+    print( df)
+    print(pd.value_counts( categories ))
+
+    ret_df = pd.dataFrame()
+    ret_df.index = labels
+    ret_df['count'] = pd.value_counts(categories)
+
+    return ret_df
 
 rutData = pd.read_csv("../testData/indexes/RUT 2010-2017.csv", sep='\t' ,parse_dates=['Date']).filter(['Date', 'Close'])
 rutData['Delta'] = rutData.Close.diff().shift(-1)
 #rutData['DeltaRelative'] = rutData.Delta / rutData.Close
 
-print(rutData['Delta'].shape)
-entries_count = rutData['Delta'].shape[0]
+# # calculate and plot autocorrelation
+# print(rutData['Delta'].shape)
+# entries_count = rutData['Delta'].shape[0]
+# print( rutData['Delta'] )
+# delta_autocorr = plot_autocorr( rutData['Delta'] )
+#
 
-print( rutData['Delta'] )
-delta_autocorr = plot_autocorr( rutData['Delta'] )
+hist_data = get_binned_data( rutData['Delta'], bin_count=10 )
+hist_data.plot( kind='barh' )
 
 #plt.show()
 #print (rutData)
