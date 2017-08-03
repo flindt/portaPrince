40795c4cbedaaf6d33835c49ac2bab60be712d80
moz <moz@local>
2017-08-03 23:36:41 +0200

    improved output

--- portaPrince/experiments/modelling_tools/gradients.py
+++ portaPrince/experiments/modelling_tools/gradients.py
@@ -41,7 +41,6 @@ if __name__ == '__main__':
 
     # read data
     pd = pandas.read_csv(datafile, sep='\t')
-    pd[columnName].plot(grid=True)
 
     # add gradients
     addGradients(pd, columnName)
