c39e1cdd235c8afccfe1f383f93e4eba81d09628
moz <moz@local>
2017-08-04 23:00:07 +0200

    code clenaup

--- portaPrince/experiments/rut_p.py
+++ portaPrince/experiments/rut_p.py
@@ -6,15 +6,9 @@ Created on 3 Aug 2017
 import pandas
 import matplotlib.pyplot as plt
 import numpy
-import matplotlib.colors as colors
 
 columnName = "Close"
 rutdatafile = "../testData/indexes/RUT 2010-2017.csv"
-
-def predict(data, startDate, n=100):
-    result = data.set_index("Date").loc[startDate:][:n]
-    
-    return result.values[:,0]/result.values[0,0]*100
 
 def create_pmap( data, bins=200 ):
     ''' using numpy, takes the series, bins them and returns a matrix with the
@@ -48,22 +42,11 @@ if __name__ == "__main__":
     # read data
     pd = pandas.read_csv(rutdatafile, sep='\t', parse_dates=['Date'])
     
-    #print pd
-    #pd[columnName].plot( grid=True)
+    print pd
+    pd[columnName].plot( grid=True)
     
     # create series        
-    dates = []
     allResults = pandas.DataFrame(index=range(no_days))
-    index = 0
-#     for i in range(2011,2016):
-#         for j in range(12):
-#             for k in range(27):
-#                 dateStr = str(i)+'-'+str(j+1)+'-'+str(k+1)
-#                 #result = predict(pd, dateStr, no_days)
-#                 result = data.set_index("Date").loc[startDate:][:no_days]
-#                 result = result.values[:,0]/result.values[0,0]*100
-#                 allResults['Series_'+str(index)] = result
-#                 index=index+1
     for i in range(pd.shape[0]-no_days):
         extract = pd[columnName][i:i+no_days].reset_index(drop=True)
         allResults["Series_%04d"%(i,)] = extract/extract.values[0]*100
@@ -74,12 +57,12 @@ if __name__ == "__main__":
     print allResults
     allResults.plot( legend=False, grid=True, alpha=0.05 )    
     
-#     # creating and shoing pmap
-#     plt.figure()
-#     
-#     p_matrix, yedges = create_pmap(allResults)
-#     print p_matrix   
-#     plot_pmap(p_matrix, yedges, vmax=170.0/1620 )
+    # creating and shoing pmap
+    plt.figure()
+     
+    p_matrix, yedges = create_pmap(allResults)
+    print p_matrix   
+    plot_pmap(p_matrix, yedges, vmax=170.0/1620 )
 
     plt.show()
     print "done"
